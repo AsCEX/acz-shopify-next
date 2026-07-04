39d9399c7245bafecc805bec64ab9605ea5f9259
Allan Cabusora <yiu.ascex@gmail.com>
2026-07-04 10:41:05 +0800

    favicons

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,13 +12,25 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#ffffff",
     icons: [
       {
-        src: "/icon-192.png",
+        src: "/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/favicon/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
       {
-        src: "/icon-512.png",
+        src: "/favicon/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
